5d3bf464110d67990af4f56dee3320eac5cc0df2
Andrew Mao <andrewyxmao@gmail.com>
2024-01-20 14:44:22 -0800

    page text added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h3>SLack Off</h3>
+      <h1>Your Ultimate Study Guide</h1>
+      <div className='container'>
+        <p>Get your lecture materials summarized, 
+          consolidated and recieve a personal study plan</p>
+        <div id='uploadwrapper'>
+          <button className='upload'>Upload</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
