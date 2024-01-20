077b8fe61aca8a4cca137659f16dcff50fea7fbd
Tyler Kerswell <tylerkerswell@gmail.com>
2024-01-20 15:01:20 -0800

    working flask server with react front-end

--- server/python-server.py
+++ server/python-server.py
@@ -1,12 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, send_from_directory
 
-@app.route('/')
-def hello_world():
-    return '''<p>gamers</p>
-            <br>
-            <br>
-            <h1>yodle</h1>'''
+app = Flask(__name__, static_folder='../frontend/dist')
+
+@app.route('/helpe')
+def yeet():
+    return '''<p>pre-defined path works</p>'''
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(use_reloader=True, port=5525, threaded=True, host="0.0.0.0")
