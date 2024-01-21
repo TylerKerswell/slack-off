c95e71a3ebdeafc47698d673c08e20e2796e9988
Andrew Mao <andrewyxmao@gmail.com>
2024-01-21 02:01:08 -0800

    tabs working 1 RAHHHH

--- frontend/src/Tabs.tsx
+++ frontend/src/Tabs.tsx
@@ -2,21 +2,66 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function SummaryPanel(props: TabPanelProps){
+  const { children, value, index , ...other} = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+
+  )
+}
+
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export function CenteredTabs() {
-    const [value, setValue] = React.useState(0);
-  
-    const handleChange = (_: React.SyntheticEvent, newValue: number) => {
-      setValue(newValue);
-    };
-  
-    return (
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className='tabs'>
       <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
         <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Summary" />
-          <Tab label="Explanation + Resources" />
-          <Tab label="Study Plan" />
+          <Tab className='tab' label="Summary"  {...a11yProps(0)}/>
+          <Tab className='tab' label="Explanation + Resources" />
+          <Tab className='tab' label="Study Plan" />
         </Tabs>
       </Box>
-    );
-  }
+      <SummaryPanel value={value} index={0}>
+      <div>
+    <h3>Topic</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, delectus. Expedita dolorum 
+      officia at est molestias quod amet dolorem excepturi laborum! Qui alias maiores consequuntur tempore aut
+       aliquam suscipit nemo.</p>
+  </div>
+      </SummaryPanel>
+    </div>
+  );
+}
