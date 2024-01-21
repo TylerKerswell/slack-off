a5774e0c279993ee6f8dc75dc7a9a4fe202551a9
lishyl26 <lishyl26@gmail.com>
2024-01-20 22:28:45 -0800

    added logo to navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css'
 import { UploadIcon } from './Links';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForward } from "@fortawesome/free-solid-svg-icons";
+
 
 import { ChangeEvent, useState } from 'react';
 // import { Loading } from './Loading';
@@ -75,7 +78,11 @@ function App() {
 
   return (
     <>
-      <div id='navbar'><h3 id="name">SLack Off</h3></div>
+
+      <div id='navbar'>
+        <FontAwesomeIcon icon={faForward} />
+        <h3 id="name">SLack Off</h3>
+      </div>
       <section>
       <h1 className='pagetitle'>Your Ultimate Study Companion</h1>
       <div className='containerrim'>
@@ -102,7 +109,7 @@ function App() {
       </div> */}
       </section>
       <section>
-        <div className='loaderwrapper'>
+        <div className='tabwrapper'>
           <CenteredTabs />
         </div>
       </section>
