b0eb8ec4ae5ffd7ec547f750ae9b75d5277eeed0
Tyler Kerswell <tylerkerswell@gmail.com>
2024-01-20 19:11:49 -0800

    added mongodb

--- server/python-server.py
+++ server/python-server.py
@@ -1,7 +1,10 @@
-import os, io, read
+import os, io, read, pymongo
 from flask import Flask, send_from_directory, request, Response
 
 app = Flask(__name__, static_folder='../frontend/dist')
+
+MONGODB_URI = "mongodb+srv://public:Yhj7nZX6EajcWNdZXs4VM2x2z2TUu4Uu2wrRoA31h244EkLnr02k3qe5R39ULZnk@cluster0.mongodb.net/slackoff"
+
 
 @app.route('/uploadPDF', methods=['POST'])
 def uploadPDF():
@@ -9,12 +12,17 @@ def uploadPDF():
         return Response("not a pdf file", status = 422, mimetype="text/plain")
     usr = request.remote_addr
     try:
+        binary_data = request.data.decode('utf-8')
+        database.files.insert_one({'_id': usr, 'data': binary_data})
+    except:
+        return Response("cant add file to mongoDB", status=500, mimetype="text/plain")
+    return Response("good shit", status=202, mimetype="text/plain")
+
+    try:
         pdf_stream = io.BytesIO(request.data)
         lecture_texts = read.read_pdf(pdf_stream)
     except:
         return Response("error reading file", status=422, mimetype="test/plain")
-    return Response("good shit", status=202, mimetype="text/plain")
-
 
 
 # Serve React App
@@ -28,4 +36,11 @@ def serve(path):
 
 
 if __name__ == '__main__':
+    try:
+        client = pymongo.MongoClient(MONGODB_URI)
+        database = client["pdfs"]
+    except:
+        print("error connecting to the database")
+        os._exit(1)
+
     app.run(use_reloader=True, port=5525, threaded=True, host="0.0.0.0")
