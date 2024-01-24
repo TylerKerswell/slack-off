97ec8c35bcbe5f6f01103a86d160fb2139d43ce9
Andrew Mao <andrewyxmao@gmail.com>
2024-01-23 16:05:37 -0800

    reverted

--- src/Links.tsx
+++
@@ -1,9 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileArrowUp } from "@fortawesome/free-solid-svg-icons";
-
-export function UploadIcon(){
-    
-    return (
-        <FontAwesomeIcon className='icon' icon={faFileArrowUp} />
-    )
-} 
