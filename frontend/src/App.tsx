52f8dd8bb573bccb9aeeceb35acc53b0d18dd262
Andrew Mao <andrewyxmao@gmail.com>
2024-01-28 22:49:38 -0800

    logo added, cleaned up frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,13 @@
 import './App.css'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faForward } from "@fortawesome/free-solid-svg-icons";
 import FileUploadSingle from './FileUploadSingle';
 import { Landing } from './Landing';
+import { Navbar } from './Navbar';
 
 function App() {
 
   return (
     <>
-      <div id='navbar'>
-        <div><FontAwesomeIcon icon={faForward} color='#004080' /></div>
-        <h2 id="space">..</h2>
-        <h3 id="name">SLack Off</h3>
-        <a href="#content">
-          <button className='navbutton'>Try me!</button>
-          </a>
-      </div>
+      <Navbar />
       <Landing />
       <section id='content'>
         <h1 className='pagetitle'>Improve your learning now!</h1>
--- frontend/src/Landing.tsx
+++ frontend/src/Landing.tsx
@@ -6,14 +6,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function Landing() {
     return (
-        <div id="pagewrapper">
-            <i><h2 className="italics">Fast forward your learning with</h2></i>
-            <div className="title">
-                <div className="icon"><FontAwesomeIcon className="iconimg" icon={faForward} color='#004080' /></div>
-                <h1>SLack Off</h1>
+        <>
+            <div id="pagewrapper">
+                <i><h2 className="italics">Fast forward your learning with</h2></i>
+                <div className="title">
+                    <div className="icon"><FontAwesomeIcon className="iconimg" icon={faForward} color='#004080' /></div>
+                    <h1>SLack Off</h1>
                 </div>
-            <h2 className="desc">your ultimate study companion</h2>
-        </div>
-    
+                <h2 className="desc">your ultimate study companion</h2>
+            </div>
+        </>
     )
 }
